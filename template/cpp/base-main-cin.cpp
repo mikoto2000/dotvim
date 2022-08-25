e073a82d1c919370f55ff8c96f5171dc8cf42c94
mikoto2000 <mikoto2000@gmail.com>
2022-08-25 18:59:01 +0900

    cpp のテンプレートを追加。

---
+++ template/cpp/base-main-cin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    // 標準入力を取得
+    unsigned short n;
+    cin >> n;
+
+    {{_cursor_}}
+
+    // 標準出力
+    cout << n << endl;
+
+    return 0;
+}
+
+
