2946c65efbe3b0c9c2a3f96040daaf22a92a5d0e
mikoto2000 <mikoto2000@gmail.com>
2024-08-21 19:50:04 +0900

    Update rust template.

--- template/rust/snip-stdin-line.rs
+++ template/rust/snip-stdin-line.rs
@@ -1,9 +1,9 @@
 use std::str::FromStr;
 
-let mut line1 = String::new();
-std::io::stdin().read_line(&mut line1).ok();
-line1 = line1.trim().parse::<String>().ok().unwrap();
-let line1: Vec<&str> = line1.split_whitespace().collect();
-let a: u32 = FromStr::from_str(&line1[0]).unwrap();
-let b: u32 = FromStr::from_str(&line1[1]).unwrap();
-let c: u32 = FromStr::from_str(&line1[2]).unwrap();
+let mut line{{_input_:lineNumber}} = String::new();
+std::io::stdin().read_line(&mut line{{_input_:lineNumber}}).ok();
+line{{_input_:lineNumber}} = line{{_input_:lineNumber}}.trim().parse::<String>().ok().unwrap();
+let line{{_input_:lineNumber}}: Vec<&str> = line{{_input_:lineNumber}}.split_whitespace().collect();
+let a: u32 = FromStr::from_str(&line{{_input_:lineNumber}}[0]).unwrap();
+let b: u32 = FromStr::from_str(&line{{_input_:lineNumber}}[1]).unwrap();
+let c: u32 = FromStr::from_str(&line{{_input_:lineNumber}}[2]).unwrap();
